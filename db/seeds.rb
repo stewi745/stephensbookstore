76f4b39520805ffc25db29abd9ba66e5ec34a65d
Stephen W. Stewart <stestewisgreat@hotmail.com>
2015-04-07 19:05:19 +0100

    Adding seed data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.delete_all
+Book.create! id: 1, title: "The Catcher in the Rye", manufacturer: "Penguin", category: "Coming of Age", price: 9.99, active: true
+Book.create! id: 2, title: "The Second Half", manufacturer: "Penguin", category: "Sport", price: 15.00, active: true
+Book.create! id: 3, title: "Animal Farm",  manufacturer: "Litte Island", category: "Political Satire", price: 5.95, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
